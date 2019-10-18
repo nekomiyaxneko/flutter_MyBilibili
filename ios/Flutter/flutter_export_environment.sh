790bbec8c86c08c55f3dcb3d82dbeb8d11b6bd45
df2019 <jcb20150301@gmail.com>
2019-10-18 23:20:55 +0800

    改正包名

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\homeWork\flutterFile\flutter_MyBilibili"
+export "FLUTTER_ROOT=I:\code\flutter\sdk\flutter_windows_v1.9.1+hotfix.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=I:\code\flutter\github\flutter_MyBilibili"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=I:\code\flutter\sdk\flutter_windows_v1.9.1+hotfix.4-stable\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
