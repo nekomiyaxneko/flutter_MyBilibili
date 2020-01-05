e7ff88536a814cef8b9d42cb5eda5e1dd5b63880
nekomiyaxneko <1434084180@qq.com>
2020-01-05 11:00:18 +0800

    Signed-off-by: nekomiyaxneko <1434084180@qq.com>

--- lib/views/danmaku/danmaku_api.dart
+++ lib/views/danmaku/danmaku_api.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class DanmakuApi{
   static getDanmakuByUrl(String url) async{
-    String yesUrl="http://lllinux.hkg03.bdysite.com/bilibili/danmaku/getDanmaku.php?url="+url;
+    String yesUrl="http://106.12.218.227/api/bilibili/getDanmaku.php?url="+url;
     Dio dio=Dio();
     try{
       Response res=await dio.get(yesUrl,options: Options(
